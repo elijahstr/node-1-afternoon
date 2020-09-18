5f04e4ccde9237d8cf083e6bbd9694d1b099ff2f
elijahstr <elijahstraight14@gmail.com>
2020-09-18 16:21:40 -0600

    update

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,17 +1,3 @@
-// const products = require("../products.json");
-
-// const getProducts = (req, res) => {
-//     if(req.query.price===true){
-//         const items = products.filter(val => val.price >= parseInt(req.query.price));
-//         return res.status(200).send(items);
-//     }
-//     res.status(200).send(products);
-// }
-
-
-
-// module.exports= getProducts;
-
 const products = require('../products.json');
 
 const getProducts = (req, res) => {
